14bb50adb42459d27cf2d8c4dfe50bb44ccb709e
Toru Nagashima <star.ctor@gmail.com>
2016-01-05 02:06:29 +0900

    stash

---
+++ tests/lib/rules/no-unpublished-import.js
@@ -0,0 +1,162 @@
+/**
+ * @author Toru Nagashima
+ * @copyright 2015 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var path = require("path"),
+    RuleTester = require("eslint").RuleTester,
+    rule = require("../../../lib/rules/no-unpublished-import");
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+/**
+ * Makes a file path to a fixture.
+ * @param {string} name - A name.
+ * @returns {string} A file path to a fixture.
+ */
+function fixture(name) {
+    return path.resolve(__dirname, "../../fixtures/no-unpublished", name);
+}
+
+//------------------------------------------------------------------------------
+// Test
+//------------------------------------------------------------------------------
+
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-unpublished-import", rule, {
+    valid: [
+        {
+            filename: fixture("test.js"),
+            code: "import eslint from 'eslint';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import eslint from 'eslint/lib/ast-utils';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import a from './a';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import a from './a.js';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import resolve from 'resolve';",
+            options: [{"publish": "*.js"}],
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import mocha from 'mocha';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+
+        // Ignores it if the filename is unknown.
+        {
+            code: "import abc from 'no-exist-package-0';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        },
+        {
+            code: "import b from './b';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"}
+        }
+    ],
+    invalid: [
+        {
+            filename: fixture("test.js"),
+            code: "import abc from 'no-exist-package-0';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: [
+                "\"no-exist-package-0\" is not found.",
+                "\"no-exist-package-0\" is not published."
+            ]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import test from '@mysticatea/test';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: [
+                "\"@mysticatea/test\" is not found.",
+                "\"@mysticatea/test\" is not published."
+            ]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import abc from 'no-exist-package-0';",
+            options: [{"publish": null}],
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: ["\"no-exist-package-0\" is not found."]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import b from './b';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: ["\"./b\" is not found."]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import a from './a.json';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: ["\"./a.json\" is not found."]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import async from 'async';",
+            env: {node: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: ["\"async\" is not published."]
+        },
+        {
+            filename: fixture("test.js"),
+            code: "import mocha from 'mocha';",
+            options: [{"publish": "*.js"}],
+            env: {node: true, es6: true},
+            ecmaFeatures: {modules: true},
+            parserOptions: {sourceType: "module"},
+            errors: ["\"mocha\" is not published."]
+        }
+    ]
+});
